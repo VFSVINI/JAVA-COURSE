013067d75a8426f82a8dc829334c261c31913c63
VFSVINI <vfs2@cin.ufpe.br>
2025-09-06 01:04:22 -0300

    Relação Entre Primitivos X Objetos. Trabalhando com Conversão de Tipos Primitivos. Trabalhando com Wrappers.

---
+++ EXERCÍCIOS/src/fundamentos/ConversaoTipoPrimitivoNumerico.java
@@ -0,0 +1,28 @@
+package fundamentos;
+
+public class ConversaoTipoPrimitivoNumerico {
+	
+	public static void main(String[] args) {
+		
+		double a = 1; // Conversão Implícita
+		System.out.println(a);
+		
+		float b = (float) 1.123456788888; // Conversão Explícita (CAST)
+		System.out.println(b);
+		
+		int c = 340;
+		byte d = (byte) c; // Conversão Explícita (CAST)
+		System.out.println(d);
+		
+		double e = 1.999999999;
+		int f = (int) e; // Conversão Explícita (CAST)
+		System.out.println(f);
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ EXERCÍCIOS/src/fundamentos/PrimitivoVsObjeto.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class PrimitivoVsObjeto {
+	
+	public static void main(String[] args) {
+		
+		String s = new String ("texto");
+		s.toUpperCase();
+		
+		// Wrappers são a Versão Objeto dos Tipos Primitivos
+		
+		int a = 123;
+		System.out.println(a);
+		
+		
+	}
+
+}
