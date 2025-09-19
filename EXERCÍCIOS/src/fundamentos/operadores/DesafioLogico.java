7bff84c98effd33e6ee8fa251a2d83ad809dbfb7
vfs2 <vfs2@P1C01.windows.cin.ufpe.br>
2025-09-19 19:39:44 -0300

    "Trabalhando com Operadores

---
+++ EXERCÍCIOS/src/fundamentos/operadores/DesafioLogico.java
@@ -0,0 +1,27 @@
+package fundamentos.operadores;
+
+public class DesafioLogico {
+	
+	public static void main (String[] args) {
+		
+		boolean trabalho1 = true;
+		boolean trabalho2 = false;
+		
+		boolean comprouTV50 = trabalho1 && trabalho2;
+		boolean comprouTV32 = trabalho1 ^ trabalho2;
+		boolean comprouSorvete = trabalho1 || trabalho2;
+		boolean maisSaudavel = !comprouSorvete; // Operador Unário!
+		
+		System.out.println("Comprou TV 50\"? " + comprouTV50);
+		System.out.println("Comprou TV 32\"? " + comprouTV32);
+		System.out.println("Comprou TV 32\"Sorverte? " + comprouSorvete);
+		
+		
+		System.out.println("Mais Saldável? " + maisSaudavel);
+		
+		
+		
+		
+	}
+
+}
