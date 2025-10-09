32cc072ebe8cd77fab77e3155a05927ee1eaebf7
vfs2 <vfs2@P1C01.windows.cin.ufpe.br>
2025-10-09 20:47:09 -0300

    Operador Ternário

---
+++ EXERCÍCIOS/src/fundamentos/operadores/Ternario.java
@@ -0,0 +1,14 @@
+package fundamentos.operadores;
+
+public class Ternario {
+	
+	public static void main (String[] args) {
+		
+		double media = 8.7;
+		String resultadoFinal = media >= 7.0 ? "aprovado." : "reprovado.";
+		
+		System.out.println("O aluno está " + resultadoFinal);
+				
+	}
+
+}
